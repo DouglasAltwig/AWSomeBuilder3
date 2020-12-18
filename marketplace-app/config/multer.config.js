eeceec369df737d45249afc471525c57512ed030
Douglas Altwig <douglas.altwig@gmail.com>
2020-12-18 16:26:14 -0300

    Hard coded variables were replace by environment variables.

---
+++ marketplace-app/config/multer.config.js
@@ -0,0 +1,46 @@
+require('dotenv').config()
+const aws = require('aws-sdk')
+const multer = require('multer')
+const multerS3 = require('multer-s3')
+
+
+aws.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION
+})
+
+const s3 = new aws.S3()
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true)
+  } else {
+    cb(new Error('Invalid file type, only JPEG and PNG is allowed'), false)
+  }
+}
+
+const upload = multer({
+  fileFilter: fileFilter,
+  limits: {
+    fileSize: 1024 * 1024 * 5 // 5 MB files
+  },
+  storage: multerS3({
+    s3: s3,
+    bucket: 'octank-marketplace',
+    // contentType: multerS3.AUTO_CONTENT_TYPE,
+    metadata: function (req, file, cb) {
+      let mdata = {
+        fieldName: file.fieldname, 
+        originalname: file.originalname, 
+        mimetype: file.mimetype
+      }
+      cb(null, mdata);
+    },
+    key: function (req, file, cb) {
+      cb(null, `${Date.now().toString()}-${file.originalname}`)
+    }
+  })
+})
+
+module.exports = upload
